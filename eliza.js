73d4904cc061317e7cbc86fc68227e56c5b7234d
rxbaker <rbaker11@asu.edu>
2016-10-24 21:36:54 -0700

    added logging

--- eliza.js
+++ eliza.js
@@ -11,8 +11,10 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+const fs = require('fs');
 
 var name = '';
+var log = '';
 var coffeeIntervalFlag = false;
 var startQuestions = [
     'how is your day going?',
@@ -46,23 +48,27 @@ rl.on('line', function (line) {
 
 
 function getResponse (line, callback) {
+    log += line+'\n';
     line = line.trim().toLowerCase();
-    //if the user wants to quit
-    if (line === 'quit') {
-        rl.close();
-        process.exit();
-    }
-    if (coffeeIntervalFlag && line === 'maybe') {
-        //the user responded quickly with maybe
-        clearInterval(coffeeInterval);
+    switch (line) {
+        case 'quit':
+            //if the user wants to quit
+            rl.close();
+            process.exit();
+        case 'maybe':
+            if (coffeeIntervalFlag) {
+                clearInterval(coffeeInterval);
+            }
+            break;
+        case '':
+            return callback('Say more than nothing!');
+        case 'log':
+            return logConversation(callback);
     }
     //set thisflag to false
     coffeeIntervalFlag = false;
 
-    if ('' === line) {
-        return callback('Say more than nothing!');
-    }
-
+    //if it's the first response they're setting their name
     if ('' === name) {
         name = line;
         var i = rand(0, startQuestions.length-1);
@@ -73,8 +79,13 @@ function getResponse (line, callback) {
     line = line.replace(/[^a-z ]/g, '');
     var words = line.split(' ');
 
+    //TODO: search for words in our lists and send an appropriate response
+
     return callback('The first word you said was '+words[0]);
 }
+
+//TODO: interval function to check for new files
+//TODO: function to read in new file into our data structures
 
 function coffeeInterval () {
     console.log();
@@ -92,6 +103,22 @@ function questionInterval () {
     say(slowPrompts[i][0]+tempName+slowPrompts[i][1]);
 }
 
+function logConversation (callback) {
+    //make sure their name has been set
+    if ('' === name) {
+        return callback("I can't log the conversation if I don't know your name. What is it?");
+    }
+
+    var filename = name+'_'+(new Date().getTime())+'.log';
+    fs.writeFile(filename, log, function (error) {
+        if (error) {
+            callback('Error logging conversation: '+error);
+        } else {
+            callback('Conversation Logged');
+        }
+    });
+}
+
 
 //////////////////////////////////////////////////////////////////////
 /// Helper functions
@@ -106,6 +133,7 @@ function setQuestionInterval () {
 }
 
 function say (message) {
+    log += 'Eliza: '+message+'\nYou: ';
     process.stdout.write('Eliza: '+message+'\nYou: ');
 }
 
